f992d737c7e866ca63a46c567d6ff40875b52e94
imasshaWe <imasha98.we@gmail.com>
2023-01-21 20:43:00 +0530

    done authentication

--- backend/src/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    console.log(req.auth);
-    res.send('Hello World!');
-});
-
-module.exports = router;
