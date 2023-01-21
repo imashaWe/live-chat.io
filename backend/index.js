f1e905848ffaee97ccb42001aa895ad0460e4deb
imasshaWe <imasha98.we@gmail.com>
2023-01-21 11:13:51 +0530

    Config Express.JS

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// set up express app
+const app = express();
+const port = process.env.PORT || 4000;
+
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+// set up body-parser middleware
+app.use(bodyParser.json());
+
+// error handling middleware
+app.use((e, req, res, next) => {
+    res.status(422).send({
+        status: false,
+        message: e.message
+    });
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+    console.log(`server listening for requests at port:${port}`);
+});
